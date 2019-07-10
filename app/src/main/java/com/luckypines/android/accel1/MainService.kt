d044ac3991a76753c9689bc4beca9cbd55b27359
Fumiaki Yoshimatsu <fumiaki@ixco.io>
2019-07-10 18:20:43 -0400

    Save data to file.

--- app/src/main/java/com/luckypines/android/accel1/MainService.kt
+++ app/src/main/java/com/luckypines/android/accel1/MainService.kt
@@ -17,6 +17,11 @@ import android.os.IBinder
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import com.google.android.gms.location.*
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStreamWriter
+import java.util.*
 import kotlin.math.abs
 
 interface OnValueChangedListener {
@@ -58,6 +63,8 @@ class MainService: Service() {
     private var accelerometer: Sensor? = null
     private var locationClient: FusedLocationProviderClient? = null
     private var binder = MainServiceBinder()
+    private var file: File? = null
+    private var writer: OutputStreamWriter? = null
 
     override fun onBind(intent: Intent?): IBinder? {
         return binder
@@ -95,15 +102,19 @@ class MainService: Service() {
             .setTicker("Ticker Message")
             .build()
         startForeground(SERVICE_ID, notification)
+        file = File.createTempFile("data.txt", null, cacheDir)
+        writer = OutputStreamWriter(FileOutputStream(file))
+        Log.d(">>>> file", file!!.absolutePath)
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        writer?.close()
         locationClient?.removeLocationUpdates(locationCallback)
         sensorManager.unregisterListener(sensorListener)
     }
 
-    val sensorListener = object: SensorEventListener {
+    val sensorListener = object : SensorEventListener {
         override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
             Log.d("hoge", "$p1")
         }
@@ -113,6 +124,12 @@ class MainService: Service() {
                 val values = event.values
                 if (abs(values[0]) > 0.1 || abs(values[1]) > 0.1 || abs(values[2]) > 0.1) {
                     binder.setData(values)
+                    try {
+                        val time = Date().time
+                        writer?.write(String.format("V, %.04f, %.04f, %.04f, %d", values[0], values[1], values[2], time))
+                    } catch (e: IOException) {
+                        Log.e(">>>v", e.localizedMessage)
+                    }
                 }
             }
         }
@@ -124,7 +141,8 @@ class MainService: Service() {
             val channel = NotificationChannel(CHANNEL_ID, "MainChannel", importance).apply {
                 description = "Main Channel"
             }
-            val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             notificationManager.createNotificationChannel(channel)
         }
     }
@@ -133,7 +151,13 @@ class MainService: Service() {
         override fun onLocationResult(locationResult: LocationResult?) {
             locationResult ?: return
             binder.setLocations(locationResult.locations)
+            try {
+                locationResult.locations.forEach { location ->
+                    writer?.write(String.format("L, %.08f, %.08f, %d", location.latitude, location.longitude, location.time))
+                }
+            } catch (e: IOException) {
+                Log.e(">>>l", e.localizedMessage)
+            }
         }
     }
-
 }
